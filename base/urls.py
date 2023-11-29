1f0d83b9d07350eb0201dc6e92fd76fd0f9c7ddc
ManvelManucharyan <manvelmanucharyan98@gmail.com>
2023-11-29 18:23:30 +0400

    ADD CRUD

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('updateRoom/<str:pk>/', views.updateRoom, name='update-room'),
+    path('deleteRoom/<str:pk>/', views.deleteRoom, name='delete-room'),
 ]
