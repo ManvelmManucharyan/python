34bb7b80715c88140a8ff9bf63141a16b80438db
ManvelManucharyan <manvelmanucharyan98@gmail.com>
2023-11-30 18:17:36 +0400

    ADD login, reg

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
+    
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
+    
     path('create-room/', views.createRoom, name='create-room'),
     path('updateRoom/<str:pk>/', views.updateRoom, name='update-room'),
     path('deleteRoom/<str:pk>/', views.deleteRoom, name='delete-room'),
